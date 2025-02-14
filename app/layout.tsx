f18d7343f974990508e78434d7d970357767a014
oscar2697 <lindooscar635@gmail.com>
2025-02-13 22:42:19 -0500

    Refactor RootLayout to use Inter font and update ClerkProvider appearance settings

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,45 @@
+import { ReactNode } from "react";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "@/app/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-import { Toaster } from "@/components/ui/toaster";
+import { Inter } from "next/font/google";
 import "@stream-io/video-react-sdk/dist/css/styles.css";
-import 'react-datepicker/dist/react-datepicker.css';
+import "react-datepicker/dist/react-datepicker.css";
+import "./globals.css";
+import { Toaster } from "@/components/ui/toaster";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "YOOM",
-  description: "Video Calling app",
-  icons: { icon: '/icons/logo.svg' }
+  description: "Video calling App",
+  icons: {
+    icon: "/icons/logo.svg",
+  },
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: Readonly<{ children: ReactNode }>) {
   return (
     <html lang="en">
-      <ClerkProvider>
-        <body className={`${geistSans.className} bg-dark-2`}>
-          {children}
+      <ClerkProvider
+        appearance={{
+          layout: {
+            socialButtonsVariant: "iconButton",
+            logoImageUrl: "/icons/yoom-logo.svg",
+          },
+          variables: {
+            colorText: "#fff",
+            colorPrimary: "#0E78F9",
+            colorBackground: "#1C1F2E",
+            colorInputBackground: "#252A41",
+            colorInputText: "#fff",
+          },
+        }}
+      >
+        <body className={`${inter.className} bg-dark-2`}>
           <Toaster />
+          {children}
         </body>
       </ClerkProvider>
     </html>
