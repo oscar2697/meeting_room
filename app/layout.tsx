b9a6e4be7c2ed63f0b0d93a2d9b4b455fc56f9d1
oscar2697 <lindooscar635@gmail.com>
2025-02-13 22:11:05 -0500

    Deploy

---
+++ app/layout.tsx
@@ -0,0 +1,58 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "@/components/ui/toaster"
+import "@stream-io/video-react-sdk/dist/css/styles.css";
+import 'react-datepicker/dist/react-datepicker.css'
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "YOOM",
+  description: "Video Calling app",
+  icons: {
+    icon: '/icons/logo.svg'
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <ClerkProvider 
+        appearance={{
+          layout: {
+            logoImageUrl: '/icons/yoom-logo.svg',
+            socialButtonsVariant: 'iconButton'
+          },
+          variables: {
+            colorText: '#fff',
+            colorPrimary: '#0E78F9',
+            colorBackground: '#1c1f2e',
+            colorInputBackground: '#252a41',
+            colorInputText: '#fff',
+          }
+        }}
+      >
+        <body
+          className={`${geistSans.className} bg-dark-2`}
+        >
+          {children}
+          <Toaster/>
+        </body>
+      </ClerkProvider>
+    </html>
+  );
+}
